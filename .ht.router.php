3b38def806224821106a467818e4ad65161b9ded
Raruto <Raruto@users.noreply.github.com>
2021-02-23 02:43:20 +0100

    fix "feed.xml" on dev server

--- .ht.router.php
+++ .ht.router.php
@@ -10,39 +10,56 @@
  */
 if (PHP_SAPI == 'cli-server') {
 
-    $path  = pathinfo($_SERVER['SCRIPT_FILENAME']);
-    $index = realpath($path['dirname'].'/index.php');
+    $url   = parse_url($_SERVER['REQUEST_URI']);
+    $ext   = pathinfo($_SERVER['SCRIPT_FILENAME'])['extension'];
     $file  = $_SERVER['SCRIPT_FILENAME'];
 
     /* "dot" routes (see: https://bugs.php.net/bug.php?id=61286) */
-    $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
+    $_SERVER['PATH_INFO'] = str_replace('/', DIRECTORY_SEPARATOR, $url['path']);
 
     /* static files (eg. assets/app/css/style.css) */
-    if (is_file($file) && $path['extension'] != 'php') {
+    if (is_file($file)) {
 
         // custom Mime Types
-        if ($path['extension'] == 'tag') {
+        if ($ext == 'tag') {
             header('Content-Type: application/javascript');
             readfile($file);
             exit;
         }
 
-        // standard Mime Types
-        return false;
+        // default Mime Types.
+        if ($ext != 'php') {
+          return false;
+        }
 
     }
 
-    /* admin routes (eg. admin/api/get/collection/posts) */
-    if ( strpos( rtrim( $_SERVER['REQUEST_URI'], '/') . '/', '/'. COCKPIT . '/' ) === 0 ) {
-      str_replace( "/". COCKPIT, '', $_SERVER['REQUEST_URI'] );
-      str_replace( "/". COCKPIT, '', $_SERVER['PATH_INFO'] );
-      include_once( is_file( $index ) ? $index : COCKPIT . '/index.php');
-      exit;
-    }
+    $index = $_SERVER['PATH_INFO'] . DIRECTORY_SEPARATOR . 'index.php';
+
+    // Trasverse directories and search for "index.php"
+    $path  = dirname($index);
+    do {
+      if (is_file(__DIR__ . $path . DIRECTORY_SEPARATOR . 'index.php')) {
+        $index = $path . DIRECTORY_SEPARATOR . 'index.php';
+        break;
+      }
+      $path = dirname($path);
+    } while ($path !== DIRECTORY_SEPARATOR && $path !== '.');
+
+    // Update $_SERVER variables to point to the correct index-file.
+    $_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'] . $index;
+    $_SERVER['SCRIPT_NAME']     = $index;
+    $_SERVER['PHP_SELF']        = $index;
 
     /* index files (eg. install/index.php) */
-    if (is_file($index)) {
-        include_once($index);
-    }
+    if (is_file($_SERVER['SCRIPT_FILENAME'])) {
 
+      include_once($_SERVER['SCRIPT_FILENAME']);
+
+      // custom "index.php"
+      if (dirname($_SERVER['SCRIPT_NAME']) != DIRECTORY_SEPARATOR) {
+        exit;
+      }
+
+    }
 }
